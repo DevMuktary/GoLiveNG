f9f47d9270e8edba3e7e418492bfce6e005b1a13
DevMuktary <mukhtarakanniabdulwaheed@gmail.com>
2026-01-17 10:25:24 +0100

    Update next.config.js

--- web-client/next.config.js
+++ web-client/next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
-    // If the variable is missing, default to localhost (prevents build error)
     const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
     return [
       {
@@ -14,3 +13,5 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// forcing a rebuild for css
