73b472c89972ec1950fe611954de884a464fa687
DevMuktary <mukhtarakanniabdulwaheed@gmail.com>
2026-01-18 22:06:07 +0100

    Update next.config.js

--- web-client/next.config.js
+++ web-client/next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${apiUrl}/:path*`,
-      },
-    ]
-  },
+  // We removed the rewrites because /api/auth is now handled internally by Next.js
 }
 
 module.exports = nextConfig
-
-// forcing a rebuild for css
